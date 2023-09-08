49d9065f2a97e22dcf2f605e40d2144f3696b778
rmsoft-alex <119843044+rmsoft-alex@users.noreply.github.com>
2023-09-08 11:30:57 +0900

    설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const FETCH_API = process.env.NEXT_PUBLIC_FETCH_API;
+const FETCH_USER_API = process.env.NEXT_PUBLIC_FETCH_USER_API;
+const FETCH_ADMIN_API = process.env.NEXT_PUBLIC_FETCH_ADMIN_API;
+const FETCH_EXCEL_API = process.env.NEXT_PUBLIC_FETCH_EXCEL_API;
+const FETCH_ALARM_API = process.env.NEXT_PUBLIC_FETCH_ALARM_API;
+
+const nextConfig = {
+  env: {
+    API: FETCH_API,
+    USER_API: FETCH_USER_API,
+    ADMIN_API: FETCH_ADMIN_API,
+    EXCEL_API: FETCH_EXCEL_API,
+    ALARM_API: FETCH_ALARM_API,
+  },
+};
+
+module.exports = nextConfig;
